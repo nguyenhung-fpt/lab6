389e1ab6f6da21001fc1f8576d5c7d0b4cc2a1d4
Your Name <hungnvth2008073@fpt.edu.vn>
2021-06-18 15:46:13 +0700

    Update Lab6

---
+++ Lesson_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lab6_T2009A_NguynVanHung
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thermostat thermostat = new Thermostat();
+            Heater heater = new Heater(60);
+
+            thermostat.OnTemperatureChange +=
+                heater.OnTemperatureChanged;
+            Heater heaterCuong = new Heater(100);
+            thermostat.OnTemperatureChange +=
+                heaterCuong.OnTemperatureChanged;
+            Cooler cooler = new Cooler(80);
+            thermostat.OnTemperatureChange +=
+                cooler.OnTemperatureChanged;
+            string temperature;
+            Console.Write("Enter temperature: ");
+            temperature = Console.ReadLine();
+            thermostat.CurrentTemperature = int.Parse(temperature);
+            Console.ReadLine();
+        }
+    }
+}
